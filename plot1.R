b923957e1a87dc1b438a9d825dfa8eb1ec5277e8
balamuruganav <avbala@gmail.com>
2016-02-14 02:12:01 +0530

    completed

---
+++ plot1.R
@@ -0,0 +1,12 @@
+
+setwd("d:/R/3/exploratory-data-analysis-w1/")
+getwd()
+dataFile <- "d:/R/3/exploratory-data-analysis-w1/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
